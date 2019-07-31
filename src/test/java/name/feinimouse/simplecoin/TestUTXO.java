5e4752a726e8e1f3fa5433e7f9e8cfbfd39e81bf
feinimouse <704602977@qq.com>
2019-07-31 09:07:19 +0800

    UTXO系统的测试

---
+++ src/test/java/name/feinimouse/simplecoin/TestUTXO.java
@@ -0,0 +1,44 @@
+package name.feinimouse.simplecoin;
+
+import name.feinimouse.simplecoin.manager.SimpleUTXOCenter;
+import name.feinimouse.simplecoin.manager.SimpleUTXOOrder;
+import name.feinimouse.utils.LoopUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
+
+public class TestUTXO extends TestCenter<UTXOBundle> {
+    private final static int LIST_SIZE = 100;
+    private final static int UTXO_SIZE = 10;
+
+    @Before @Override
+    public void setUp() {
+        transList = LoopUtils.loopToList(LIST_SIZE, () -> transGen.genUTXOBundle(UTXO_SIZE));
+        var order = new SimpleUTXOOrder(userManager, transList);
+        center = new SimpleUTXOCenter(order);
+        order.isOutBlock(false);
+        super.order = order;
+    }
+    
+    @Test @Override
+    public void testOrder() throws ExecutionException, InterruptedException {
+        var executor = Executors.newSingleThreadExecutor();
+        var orderRes = executor.submit(order::activate);
+        Thread.sleep(500);
+        var verifyTime = orderRes.get();
+        System.out.printf("验证 %d 条UTXO共花费：%f s \n", transList.size(), verifyTime / 1000000000f);
+    }
+
+    @Test
+    public void testWrite() {
+        write();
+    }
+
+    @Test
+    public void testCenter() {
+        runCenter();
+    }
+}
