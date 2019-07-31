0e0fd210fb57820c4a2e96bf06447e5614c3a2ac
feinimouse <cyhkkha@qq.com>
2019-07-31 16:15:25 +0800

    随机用户初始化，增加收集center信息的方法

--- src/main/java/name/feinimouse/simplecoin/Config.java
+++ src/main/java/name/feinimouse/simplecoin/Config.java
@@ -6,22 +6,30 @@ import name.feinimouse.feinism2.SM2Generator;
 import name.feinimouse.simplecoin.block.SimpleHashObj;
 import name.feinimouse.simplecoin.core.TransactionGen;
 import name.feinimouse.simplecoin.core.UserManager;
+import name.feinimouse.simplecoin.manager.SimpleCenter;
 import name.feinimouse.simplecoin.manager.SimpleVerifier;
+import name.feinimouse.utils.LoopUtils;
 import org.bson.Document;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 public abstract class Config {
-    protected final static String[] USERS = {"kasumi", "arisa", "tae", "rimi", "saaya", "yukina", "sayo", "risa", "rinko", "ako"};
+    protected static final int USER_COUNT = 100;
     protected static UserManager userManager;
     protected static TransactionGen transGen;
     protected static SimpleVerifier verifier;
     protected static SM2 sm2;
 
     public static void init() {
+        var random = new Random();
+        var USERS = LoopUtils.loopToList(USER_COUNT, () -> {
+            var l = random.nextInt(1000) + System.nanoTime();
+            return String.valueOf(l);
+        });
         sm2 = SM2Generator.getInstance().generateSM2();
-        userManager = new UserManager(USERS);
+        userManager = new UserManager(USERS.toArray(String[]::new));
         transGen = new TransactionGen(userManager);
         verifier = new SimpleVerifier(userManager);
     }
@@ -47,6 +55,17 @@ public abstract class Config {
     
     public static List<Document> transform(List<Transaction> list) {
         return list.stream().map(t -> new SimpleHashObj(t).toDocument()).collect(Collectors.toList());
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static void collectCenter(SimpleCenter center) {
+        System.out.println("---------------------------");
+        System.out.println(center.getName());
+        System.out.printf("共出块：%d 个\n", center.getBlockCounts());
+        System.out.printf("运行总时间：%f s\n", center.getRunTime() / 1000000000f);
+        System.out.printf("验证总时间：%f s\n", center.getVerifyTime() / 1000000000f);
+        collectTime(center.getSaveTimes(), "出块");
+        System.out.println("---------------------------");
     }
     
     public interface ConfigRunner {
