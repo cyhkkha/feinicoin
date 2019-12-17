ce6e4ae0975a3d8431ef23dcedfa6ae11fbb41ba
feinimouse <704602977@qq.com>
2019-12-17 20:17:43 +0800

    提供数组转化为json的便捷方法

--- src/main/java/name/feinimouse/feinicoinplus/core/block/Jsobj.java
+++ src/main/java/name/feinimouse/feinicoinplus/core/block/Jsobj.java
@@ -1,11 +1,21 @@
 package name.feinimouse.feinicoinplus.core.block;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.List;
+
 public abstract class Jsobj {
-    public abstract JSONObject toJson();
+    public JSONObject toJson() {
+        return new JSONObject(this);
+    }
     @Override
     public String toString() {
         return toJson().toString();
+    }
+    public static <T extends Jsobj> JSONArray genJson(List<T> list) {
+        JSONArray ja = new JSONArray();
+        list.forEach(jo -> ja.put(jo.toJson()));
+        return ja;
     }
 }
