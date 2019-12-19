cc5dbecc0b71eb77b9162a3c4ac71062cb10c4c0
feinimouse <704602977@qq.com>
2019-12-19 17:37:38 +0800

    spring配置更改

--- src/main/java/name/feinimouse/feinicoinplus/Config.java
+++ src/main/java/name/feinimouse/feinicoinplus/Config.java
@@ -1,9 +1,9 @@
 package name.feinimouse.feinicoinplus;
 
 import name.feinimouse.feinicoinplus.core.crypt.HashGen;
-import name.feinimouse.feinicoinplus.core.crypt.HashGenImpl;
+import name.feinimouse.feinicoinplus.core.crypt.MurmurHashGen;
 import name.feinimouse.feinicoinplus.core.crypt.SignGen;
-import name.feinimouse.feinicoinplus.core.crypt.SignGenImpl;
+import name.feinimouse.feinicoinplus.core.crypt.SM2SignGen;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,11 +19,11 @@ public class Config {
     
     @Bean
     public HashGen hashGen() {
-        return new HashGenImpl(hashSeed, hashLong);
+        return new MurmurHashGen(hashSeed, hashLong);
     }
     
     @Bean
     public SignGen signGen() throws Exception {
-        return new SignGenImpl();
+        return new SM2SignGen();
     }
 }
