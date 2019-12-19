c33df19130f22d17b55ac31b46df0d36344e72e7
feinimouse <704602977@qq.com>
2019-12-19 17:38:06 +0800

    复合哈希对象

---
+++ src/main/java/name/feinimouse/feinicoinplus/core/obj/MultiHashObj.java
@@ -0,0 +1,40 @@
+package name.feinimouse.feinicoinplus.core.obj;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
+public class MultiHashObj extends HashObj {
+    private Map<String, HashObj> map;
+    private String[] hashTree;
+    
+    public MultiHashObj(Map<String, HashObj> map, String[] hashTree) {
+        this.map = map;
+        this.hash = hashTree[0];
+        this.hashTree = hashTree;
+    }
+    
+    public HashObj get(String key) {
+        return map.get(key);
+    }
+    
+    @Override
+    public String summary() {
+        return hash;
+    }
+
+    @Override
+    public Map<String, HashObj> getObject() {
+        return map;
+    }
+
+    @Override
+    public JSONObject json() {
+        return new JSONObject().put("obj", JsonObj.genJson(map)).put("hash", hash);
+    }
+
+    public String[] hashTree() {
+        return hashTree;
+    }
+    
+}
