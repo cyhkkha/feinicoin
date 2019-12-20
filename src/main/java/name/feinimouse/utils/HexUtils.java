34df31f090f433e7cf9c506936625a018b58caec
feinimouse <704602977@qq.com>
2019-12-20 14:23:38 +0800

    新增16位随机数生成工具

--- src/main/java/name/feinimouse/utils/HexUtils.java
+++ src/main/java/name/feinimouse/utils/HexUtils.java
@@ -1,5 +1,7 @@
 package name.feinimouse.utils;
 
+import java.util.Random;
+
 public class HexUtils {
     public static String byteToHex(byte[] bytes) {
         var buf = new StringBuilder();
@@ -20,5 +22,33 @@ public class HexUtils {
             bytes[i] = (byte) Integer.parseInt(sub, 16);
         }
         return bytes;
+    }
+    
+    // 随机生成xx位的随机十六进制数
+    public static String randomHexString(int place) {
+        if (place <= 0) {
+            return "0";
+        }
+        if (place <= 7) {
+            return randomHex(place);
+        }
+        StringBuilder result = new StringBuilder().append(randomHex(place % 7));
+        for (int i = 0; i < place / 7; i++) {
+            result.append(randomHex(7));
+        }
+        return result.toString();
+    }
+    
+    private static String randomHex(int place) {
+        if (place <= 0) {
+            return "";
+        }
+        int max = 1;
+        for (int i = 0; i < place; i++) {
+            max *= 16;
+        }
+        int min = max / 16;
+        int scope = max - min;
+        return Integer.toHexString(new Random().nextInt(scope) + min);
     }
 }
