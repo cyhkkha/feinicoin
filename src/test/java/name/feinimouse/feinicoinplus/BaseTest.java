09c6debf127296e878c1c3feb7f8d27a9a62d3ef
feinimouse <704602977@qq.com>
2019-12-23 17:21:02 +0800

    temp

--- src/test/java/name/feinimouse/feinicoinplus/BaseTest.java
+++ src/test/java/name/feinimouse/feinicoinplus/BaseTest.java
@@ -1,27 +1,28 @@
 package name.feinimouse.feinicoinplus;
 
+import name.feinimouse.feinicoinplus.core.BaseSignObj;
 import name.feinimouse.feinicoinplus.core.block.Transaction;
 import name.feinimouse.feinicoinplus.core.BaseHashObj;
-import name.feinimouse.utils.HexUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class BaseTest {
     @Test
     public void testJava() {
-        // 100000 - FFFFF
-        int test = new Random().nextInt(983040) + 65536;
-        System.out.println(Integer.toString(test, 16));
-        System.out.println(Integer.toHexString(test));
-        System.out.println(HexUtils.randomHexString(16));
-        
     }
     
     @Test
     public void testGeneric() {
         BaseHashObj<Transaction> obj = new BaseHashObj<>(new Transaction(), "ssss", "hhhhh");
+        BaseHashObj<Transaction> obj2 = new BaseHashObj<>(new Transaction(), "ssss", "hhhhh");
+        BaseSignObj<Transaction> obj1 = new BaseSignObj<>(obj2);
+        ConcurrentHashMap<Class<?>, String> map = new ConcurrentHashMap<>();
+        map.put(obj.obj().getClass(), "ssss");
+        Assert.assertTrue(map.containsKey(obj1.obj().getClass()));
+        System.out.println(map.get(obj1.obj().getClass()));
         Assert.assertEquals(obj.obj().getClass(), Transaction.class);
+        
     }
 }
