ee971b469fa014ccd3ecc5eb5b22b37713e3d2aa
feinimouse <704602977@qq.com>
2019-12-23 17:24:18 +0800

    完成verifier

--- src/main/java/name/feinimouse/feinicoinplus/core/node/Verifier.java
+++ src/main/java/name/feinimouse/feinicoinplus/core/node/Verifier.java
@@ -1,10 +1,19 @@
 package name.feinimouse.feinicoinplus.core.node;
 
+import lombok.Getter;
+import lombok.Setter;
+import name.feinimouse.feinicoinplus.core.SignObj;
+import name.feinimouse.feinicoinplus.core.block.AssetTrans;
+import name.feinimouse.feinicoinplus.core.block.Transaction;
+import name.feinimouse.feinicoinplus.core.crypt.SignGen;
 import name.feinimouse.feinicoinplus.core.node.exce.BadCommitException;
+import name.feinimouse.feinicoinplus.core.node.exce.InvalidStartException;
+import name.feinimouse.feinicoinplus.core.node.exce.NodeRunningException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 import java.security.PrivateKey;
+import java.security.PublicKey;
 
 // verifier基类
 @Component("verifier")
@@ -12,26 +21,71 @@ public class Verifier extends CacheNode {
 
     // 签名串
     protected PrivateKey privateKey;
+    // 公钥仓库
+    @Setter @Getter
+    protected PublicKeyHub publicKeyHub;
+    // 签名机
+    @Setter @Getter
+    protected SignGen signGen;
     
     // 节点类型为Verifier
     public Verifier(PrivateKey privateKey) {
-        super("verifier");
+        super("verifier", new Class[]{ Transaction.class, AssetTrans.class });
         this.privateKey = privateKey;
     }
 
     @Override
-    protected void resolveMassage() {
-        
+    protected boolean resolveMassage(JSONObject json) {
+        return false;
     }
 
     @Override
-    protected void resolveTransaction() {
-
+    protected int beforeCache(JSONObject attach) {
+        if (!attach.getString("nodeType").equals("order")) {
+            return BAD_COMMUNICATOR;
+        }
+        if (!attach.has("address")) {
+            return BAD_COMMUNICATOR;
+        }
+        return super.beforeCache(attach);
     }
 
     @Override
-    protected void resolveAssetTrans() {
+    protected boolean resolveWait() {
+        if (cacheWait.hasObject(Transaction.class)) {
+            SignAttachObj<Transaction> signAttachObj = cacheWait.get(Transaction.class);
+            if (signAttachObj != null) {
+                String callbackAddress = signAttachObj.getAttach().getString("address");
+                SignObj<Transaction> signObj = signAttachObj.getObj();
+                String signer = signObj.obj().getSender();
+                verifyAndSendBack(signer, signObj, callbackAddress, Transaction.class);
+            }
+        }
+        if (cacheWait.hasObject(AssetTrans.class)) {
+            SignAttachObj<AssetTrans> signAttachObj = cacheWait.get(AssetTrans.class);
+            if (signAttachObj != null) {
+                String callbackAddress = signAttachObj.getAttach().getString("address");
+                SignObj<AssetTrans> signObj = signAttachObj.getObj();
+                String signer = signObj.obj().getOperator();
+                verifyAndSendBack(signer, signObj, callbackAddress, AssetTrans.class);
+            }
+        }
+        return true;
+    }
+    
+    private <T> void verifyAndSendBack(String signer, SignObj<T> signObj, String address, Class<T> tClass) {
+        PublicKey key = publicKeyHub.getKey(signer);
+        boolean result = signGen.verify(key, signObj, signer);
+        signGen.sign(privateKey, signObj, address);
+        JSONObject backAttach = nodeMsg().put("type", MSG_VERIFIER_CALLBACK).put("result", result);
+        SignAttachObj<T> callback = new SignAttachObj<>(signObj, backAttach);
+        network.commit(address, callback, tClass);
+    }
 
+    // 空窗期继续运行
+    @Override
+    protected boolean resolveGapPeriod() {
+        return true;
     }
 
     @Override
@@ -45,7 +99,14 @@ public class Verifier extends CacheNode {
     }
 
     @Override
-    protected void beforeWork() {
+    public synchronized int commit(JSONObject json) {
+        return METHOD_NOT_SUPPORT;
+    }
 
+    @Override
+    protected void beforeWork() throws InvalidStartException {
+        if (signGen == null || publicKeyHub == null) {
+            throw new InvalidStartException("verifier has not been set a signGen or a publicKekHub");
+        }
     }
 }
