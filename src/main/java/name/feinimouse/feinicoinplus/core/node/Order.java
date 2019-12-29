53f55f03cfa77ce58f2a9319869204a048880893
feinimouse <704602977@qq.com>
2019-12-29 22:43:59 +0800

    优化类型检查，大幅削减重复代码

--- src/main/java/name/feinimouse/feinicoinplus/core/node/Order.java
+++ src/main/java/name/feinimouse/feinicoinplus/core/node/Order.java
@@ -2,17 +2,14 @@ package name.feinimouse.feinicoinplus.core.node;
 
 import lombok.Getter;
 import lombok.Setter;
-import name.feinimouse.feinicoinplus.core.SignObj;
 import name.feinimouse.feinicoinplus.core.block.AssetTrans;
 import name.feinimouse.feinicoinplus.core.block.Transaction;
-import name.feinimouse.feinicoinplus.core.data.Carrier;
-import name.feinimouse.feinicoinplus.core.data.CarrierSubCMC;
+import name.feinimouse.feinicoinplus.core.data.*;
 import name.feinimouse.feinicoinplus.core.exception.BadCommitException;
 import name.feinimouse.feinicoinplus.core.exception.NodeRunningException;
 import name.feinimouse.utils.ClassMapContainer;
 import name.feinimouse.utils.OverFlowException;
 import name.feinimouse.utils.UnrecognizedClassException;
-import org.json.JSONObject;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -33,28 +30,32 @@ public class Order extends CacheNode {
 
     public Order() {
         // 缓存的默认容量是30
-        super(NODE_ORDER, new CarrierSubCMC(new Class[]{Transaction.class, AssetTrans.class}, 30));
+        super(NODE_ORDER, new CarrierAttachCMC(new Class[]{Transaction.class, AssetTrans.class}, 30));
         // 供拉取的缓存默认容量是无限
-        fetchWait = new CarrierSubCMC(new Class[]{Transaction.class, AssetTrans.class});
+        fetchWait = new CarrierFetchCMC(new Class[]{Transaction.class, AssetTrans.class});
         verifyWait = new ConcurrentHashMap<>();
     }
 
     @Override
-    protected Carrier resolveFetch(Carrier carrier) throws BadCommitException {
-        if (carrier.notMatchFetch(NODE_CENTER, MSG_FETCH_ORDER, Transaction.class)
-        || carrier.notMatchFetch(NODE_CENTER, MSG_FETCH_ORDER, AssetTrans.class)) {
-            throw BadCommitException.classNotSupportException(carrier, this);
-        }
+    protected Carrier resolveFetch(Carrier carrier) {
         return fetchWait.poll(carrier.getFetchClass());
     }
 
     @Override
-    protected void beforeCache(Carrier carrier) throws BadCommitException {
-        if (carrier.notMatchAttach(NODE_ENTER, MSG_COMMIT_ORDER, SignObj.class)
-            && carrier.notMatchAttach(NODE_VERIFIER, MSG_CALLBACK_VERIFIER, SignObj.class)) {
-            throw BadCommitException.classNotSupportException(carrier, this);
+    protected void beforeCommit(Carrier carrier) throws BadCommitException {
+        NodeMessage nodeMessage = carrier.getNodeMessage();
+        if (nodeMessage.notMatch(NODE_ENTER, MSG_COMMIT_ORDER)
+            && nodeMessage.notMatch(NODE_VERIFIER, MSG_CALLBACK_VERIFIER)) {
+            throw BadCommitException.commitNotSupportException(this, nodeMessage);
         }
+    }
 
+    @Override
+    protected void beforeFetch(Carrier carrier) throws BadCommitException {
+        if (carrier.notMatchFetch(NODE_CENTER, MSG_FETCH_ORDER, Transaction.class)
+            || carrier.notMatchFetch(NODE_CENTER, MSG_FETCH_ORDER, AssetTrans.class)) {
+            throw BadCommitException.classNotSupportException(this, carrier.getFetchClass());
+        }
     }
 
     @Override
@@ -77,44 +78,45 @@ public class Order extends CacheNode {
     protected void resolveCache() {
         if (cacheWait.hasObject(Transaction.class)) {
             Carrier carrier = cacheWait.poll(Transaction.class);
-            if (carrier != null) {
-                if (carrier.getMsgType() == MSG_COMMIT_ORDER) {
-                    recordToVerify(carrier);
-                }
-                if (carrier.getMsgType() == MSG_CALLBACK_VERIFIER) {
-                    resolveVerifyCallback(carrier);
-                }
-            }
+            resolveCarrier(carrier);
         }
         if (cacheWait.hasObject(AssetTrans.class)) {
             Carrier carrier = cacheWait.poll(AssetTrans.class);
-            if (carrier != null) {
-                if (carrier.getMsgType() == MSG_COMMIT_ORDER) {
-                    recordToVerify(carrier);
-                }
-                if (carrier.getMsgType() == MSG_CALLBACK_VERIFIER) {
-                    resolveVerifyCallback(carrier);
-                }
+            resolveCarrier(carrier);
+        }
+    }
+    
+    protected void resolveCarrier(Carrier carrier) {
+        if (carrier != null) {
+            NodeMessage nodeMessage = carrier.getNodeMessage();
+            if (nodeMessage.getMsgType() == MSG_COMMIT_ORDER) {
+                recordToVerify(carrier);
+            }
+            if (nodeMessage.getMsgType() == MSG_CALLBACK_VERIFIER) {
+                resolveVerifyCallback(carrier);
             }
         }
     }
 
     protected void recordToVerify(Carrier carrier) {
-        SignObj<?> signObj = (SignObj<?>) carrier.getAttach();
+        Packer packer = carrier.getPacker();
         // 对于重复交易直接忽略
-        if (verifyWait.containsKey(signObj.gainHash())) {
+        if (verifyWait.containsKey(packer.gainHash())) {
             return;
         }
         // 存入verifier备案
-        verifyWait.put(signObj.gainHash(), carrier);
+        verifyWait.put(packer.gainHash(), carrier);
+        AttachMessage attachMessage = carrier.getAttachMessage();
+        Carrier nextCarrier = genCarrier(verifiersAddress, MSG_COMMIT_VERIFIER, attachMessage);
         // 提交给verifier集群
-        commitToNetwork(verifiersAddress, MSG_COMMIT_VERIFIER,
-            null, carrier);
+        commitToNetwork(nextCarrier, packer);
     }
 
     protected void resolveVerifyCallback(Carrier carrier) {
-        SignObj<?> signObj = (SignObj<?>) carrier.getAttach();
-        String hash = signObj.gainHash();
+        Packer packer = carrier.getPacker();
+        
+        // 查看是否备案
+        String hash = packer.gainHash();
         Carrier origin = verifyWait.get(hash);
         // 若没有含verify的备案状态，则直接丢弃
         if (origin == null) {
@@ -122,38 +124,46 @@ public class Order extends CacheNode {
         }
         // 删除该备案状态
         verifyWait.remove(hash);
-        if (signObj.getSign(carrier.getSender()) == null
-            || !carrier.getMsg().has("result")) {
-            // 如检测到verifier未进行验证则从新走验证流程
+        
+        // 查看是否为已验证交易
+        AttachMessage attachMessage = carrier.getAttachMessage();
+        // 没有标识验证者则尝试从新走验证流程，若缓存溢出则丢弃
+        if (attachMessage.getVerifier() == null) {
             try {
                 cacheWait.put(origin);
             } catch (UnrecognizedClassException | OverFlowException ex) {
-                // 如果缓存已满则只好从新提出交易请求
-                commitToNetwork(origin.getSender(), MSG_CALLBACK_ORDER,
-                    new JSONObject().put("error", "The system is busy, please try again later"), origin);
+                ex.printStackTrace();
+                sendBackError();
             }
             return;
         }
-        if (!carrier.getMsg().getBoolean("result")) {
-            // 若验证为非法交易则返回原因
-            commitToNetwork(origin.getSender(), MSG_CALLBACK_ORDER, carrier.getMsg(), origin);
+        // 如检测到verifier，但没有验证结果和签名，则证明是非法交易
+        if (attachMessage.getVerifiedResult() == null 
+            ||packer.getSign(attachMessage.getVerifier()) == null) {
+            sendBackError();
             return;
         }
+        // 若验证为非法交易则返回原因
+        if (!attachMessage.getVerifiedResult()) {
+            sendBackError();
+            return;
+        }
+        
         try {
-            // 所有验证通过进入fetch队列
-            carrier.setSender(address);
-            carrier.setNetwork(network.getAddress());
-            carrier.setNodeType(nodeType);
-            carrier.setMsg(new JSONObject()
-                .put("verifier", carrier.getSender())
-                .put("order", address)
-                .put("enter", origin.getSender()));
+            attachMessage.setOrder(address);
+            if (attachMessage.getEnter() == null) {
+                attachMessage.setEnter(origin.getAttachMessage().getEnter());
+            }
+            Carrier nextCarrier = genCarrier(null, MSG_FETCH_ORDER, attachMessage);
+            nextCarrier.setPacker(packer);
             fetchWait.put(carrier);
         } catch (UnrecognizedClassException | OverFlowException e) {
             // 理论上该错误不会发生
             e.printStackTrace();
         }
     }
+    
+    protected void sendBackError() {}
     
     @Override
     protected void resolveGapPeriod() {
