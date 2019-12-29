c62f56b6f3bbaaf6e57e60937f7087b4901641f5
feinimouse <704602977@qq.com>
2019-12-29 22:46:22 +0800

    旧代码删除

--- src/test/java/name/feinimouse/feinicoinplus/BaseTest.java
+++ src/test/java/name/feinimouse/feinicoinplus/BaseTest.java
@@ -1,12 +1,6 @@
 package name.feinimouse.feinicoinplus;
 
-import name.feinimouse.feinicoinplus.core.data.BaseSignObj;
-import name.feinimouse.feinicoinplus.core.block.Transaction;
-import name.feinimouse.feinicoinplus.core.data.BaseHashObj;
-import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.concurrent.ConcurrentHashMap;
 
 public class BaseTest {
     @Test
@@ -15,14 +9,5 @@ public class BaseTest {
     
     @Test
     public void testGeneric() {
-        BaseHashObj<Transaction> obj = new BaseHashObj<>(new Transaction(), "ssss", "hhhhh");
-        BaseHashObj<Transaction> obj2 = new BaseHashObj<>(new Transaction(), "ssss", "hhhhh");
-        BaseSignObj<Transaction> obj1 = new BaseSignObj<>(obj2);
-        ConcurrentHashMap<Class<?>, String> map = new ConcurrentHashMap<>();
-        map.put(obj.obj().getClass(), "ssss");
-        Assert.assertTrue(map.containsKey(obj1.obj().getClass()));
-        System.out.println(map.get(obj1.obj().getClass()));
-        Assert.assertEquals(obj.obj().getClass(), Transaction.class);
-        
     }
 }
