50970518523f030d014d9525e1f085a3e312781a
feinimouse <704602977@qq.com>
2019-12-29 22:42:27 +0800

    优化类型检查，大幅削减重复代码

--- src/main/java/name/feinimouse/feinicoinplus/core/node/Verifier.java
+++ src/main/java/name/feinimouse/feinicoinplus/core/node/Verifier.java
@@ -3,15 +3,14 @@ package name.feinimouse.feinicoinplus.core.node;
 import lombok.Getter;
 import lombok.Setter;
 import name.feinimouse.feinicoinplus.core.PublicKeyHub;
-import name.feinimouse.feinicoinplus.core.SignObj;
 import name.feinimouse.feinicoinplus.core.block.AssetTrans;
 import name.feinimouse.feinicoinplus.core.block.Transaction;
 import name.feinimouse.feinicoinplus.core.SignGen;
-import name.feinimouse.feinicoinplus.core.data.Carrier;
-import name.feinimouse.feinicoinplus.core.data.CarrierSubCMC;
+import name.feinimouse.feinicoinplus.core.data.*;
 import name.feinimouse.feinicoinplus.core.exception.BadCommitException;
 import name.feinimouse.feinicoinplus.core.exception.NodeRunningException;
-import org.json.JSONObject;
+import name.feinimouse.feinicoinplus.core.lambda.InOutRunner;
+
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -28,54 +27,57 @@ public class Verifier extends CacheNode {
     // 签名机
     @Setter @Getter
     protected SignGen signGen;
-    
+
     // 节点类型为Verifier
     public Verifier(PrivateKey privateKey) {
         // 默认缓存的初始容量为30
-        super(NODE_VERIFIER, new CarrierSubCMC(new Class[]{ Transaction.class, AssetTrans.class }, 30));
+        super(NODE_VERIFIER, new CarrierAttachCMC(new Class[]{ Transaction.class, AssetTrans.class }, 30));
         this.privateKey = privateKey;
     }
 
     @Override
-    protected void beforeCache(Carrier carrier) throws BadCommitException {
-        if (carrier.notMatchAttach(NODE_ORDER, MSG_COMMIT_VERIFIER, SignObj.class)) {
-            throw BadCommitException.classNotSupportException(carrier, this);
+    protected void beforeCommit(Carrier carrier) throws BadCommitException {
+        NodeMessage nodeMessage = carrier.getNodeMessage();
+        if (nodeMessage.notMatch(NODE_ORDER, MSG_COMMIT_VERIFIER)) {
+            throw BadCommitException.commitNotSupportException(this, nodeMessage);
         }
+    }
+
+    @Override
+    protected void beforeFetch(Carrier carrier) throws BadCommitException {
+        throw new BadCommitException("fetch not support: " + nodeMsg().toString());
     }
 
     @Override
     protected void resolveCache() {
         if (cacheWait.hasObject(Transaction.class)) {
             Carrier carrier = cacheWait.poll(Transaction.class);
-            if (carrier != null) {
-                @SuppressWarnings("unchecked")
-                SignObj<Transaction> signObj = (SignObj<Transaction>) carrier.getAttach();
-                String callbackAddress = carrier.getSender();
-                String signer = signObj.obj().getSender();
-                JSONObject result = getVerifyResultAndSign(signer, signObj);
-                commitToNetwork(callbackAddress, MSG_CALLBACK_VERIFIER, result, carrier);
-            }
+            resolveVerify(carrier, packer -> ((Transaction) packer.obj()).getSender());
         }
         if (cacheWait.hasObject(AssetTrans.class)) {
             Carrier carrier  = cacheWait.poll(AssetTrans.class);
-            if (carrier != null) {
-                @SuppressWarnings("unchecked")
-                SignObj<AssetTrans> signObj = (SignObj<AssetTrans>) carrier.getAttach();
-                String callbackAddress = carrier.getSender();
-                String signer = signObj.obj().getOperator();
-                JSONObject result = getVerifyResultAndSign(signer, signObj);
-                commitToNetwork(callbackAddress, MSG_CALLBACK_VERIFIER, result, carrier);
-            }
+            resolveVerify(carrier, packer -> ((AssetTrans) packer.obj()).getOperator());
         }
     }
-    
-    private JSONObject getVerifyResultAndSign(String signer, SignObj<?> signObj) {
-        PublicKey key = publicKeyHub.getKey(signer);
-        JSONObject result = new JSONObject().put("result", signGen.verify(key, signObj, signer));
-        signGen.sign(privateKey, signObj, address);
-        return result;
+
+    protected void resolveVerify(Carrier carrier, InOutRunner<Packer, String> runner) {
+        if (carrier != null) {
+            Packer packer = carrier.getPacker();
+            AttachMessage attachMessage = carrier.getAttachMessage();
+            NodeMessage nodeMessage = carrier.getNodeMessage();
+            String callbackAddress = nodeMessage.getCallback();
+            String signer = runner.run(packer);
+            // 验证并签名
+            attachMessage.setVerifier(address);
+            PublicKey key = publicKeyHub.getKey(signer);
+            attachMessage.setVerifiedResult(signGen.verify(key, packer, signer));
+            signGen.sign(privateKey, packer, address);
+            // 回调给Order
+            Carrier nextCarrier = genCarrier(callbackAddress, MSG_CALLBACK_VERIFIER, attachMessage);
+            commitToNetwork(nextCarrier, packer);
+        }
     }
-    
+
 
     // 空窗期继续运行
     @Override
@@ -86,7 +88,7 @@ public class Verifier extends CacheNode {
     protected Carrier resolveFetch(Carrier carrier) throws BadCommitException {
         throw new BadCommitException("fetch not support: " + nodeMsg().toString());
     }
-    
+
 
     @Override
     protected void beforeWork() throws NodeRunningException {
