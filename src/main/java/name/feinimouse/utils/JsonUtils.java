b3a71dab87a1774d0f2df8ea2a78086650b111c5
feinimouse <704602977@qq.com>
2019-12-29 22:44:26 +0800

    新增从队列生成json

--- src/main/java/name/feinimouse/utils/JsonUtils.java
+++ src/main/java/name/feinimouse/utils/JsonUtils.java
@@ -1,28 +1,35 @@
 package name.feinimouse.utils;
 
-import name.feinimouse.feinicoinplus.core.BaseObj;
+import name.feinimouse.feinicoinplus.core.JsonAble;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 public class JsonUtils {
-    public static <T extends BaseObj> JSONArray genJson(List<T> list) {
+    public static <T extends JsonAble> JSONArray genJson(List<T> list) {
         JSONArray ja = new JSONArray();
         list.forEach(jf -> ja.put(jf.json()));
         return ja;
     }
-    public static <T extends BaseObj> JSONArray genJson(T[] arr) {
+    public static <T extends JsonAble> JSONArray genJson(T[] arr) {
         JSONArray ja = new JSONArray();
-        for (BaseObj jf : arr) {
+        for (JsonAble jf : arr) {
             ja.put(jf.json());
         }
         return ja;
     }
-    public static <T extends BaseObj> JSONObject genJson(Map<String, T> map) {
+    public static <T extends JsonAble> JSONObject genJson(Map<String, T> map) {
         JSONObject json = new JSONObject();
         map.forEach((key, value) -> json.put(key, value.json()));
         return json;
+    }
+    
+    public static <T extends JsonAble> JSONArray genJson(Queue<T> queue) {
+        JSONArray ja = new JSONArray();
+        queue.forEach(json -> ja.put(json.json()));
+        return ja;
     }
 }
