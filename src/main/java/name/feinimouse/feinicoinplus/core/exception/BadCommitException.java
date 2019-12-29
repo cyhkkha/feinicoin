922b099f71c06984aceba584988b43d2e81d32ea
feinimouse <704602977@qq.com>
2019-12-29 22:21:35 +0800

    异常类型优化

--- src/main/java/name/feinimouse/feinicoinplus/core/exception/BadCommitException.java
+++ src/main/java/name/feinimouse/feinicoinplus/core/exception/BadCommitException.java
@@ -2,7 +2,6 @@ package name.feinimouse.feinicoinplus.core.exception;
 
 import name.feinimouse.feinicoinplus.core.Node;
 import name.feinimouse.feinicoinplus.core.data.NodeMessage;
-import name.feinimouse.feinicoinplus.core.data.Packer;
 
 public class BadCommitException extends Exception {
     public BadCommitException() {
@@ -17,19 +16,31 @@ public class BadCommitException extends Exception {
         return new BadCommitException("Node Dose Not Work, Commit Failed from: " + node.nodeMsg().toString());
     }
     
-    public static BadCommitException noSenderException(Node node, NodeMessage nodeMessage) {
+    public static BadCommitException noSenderException(Node node) {
         return new BadCommitException(
-            "Commit without origin: " + nodeMessage.json().toString() + 
-                ", Commit Failed from: " + node.nodeMsg().toString());
+            "Commit without origin, and commit Failed from: " + node.nodeMsg().toString());
+    }
+
+    public static BadCommitException commitOverflowException(Node node) {
+        return new BadCommitException(
+            "Commit Failed, Node cache overflow from: " + node.nodeMsg().toString());
+    }
+
+    public static BadCommitException classNotSupportException(Node node, Class<?> aClass) {
+        if (aClass == null) {
+            return new BadCommitException("Null operation class type, operation failed from: " + node.nodeMsg().toString());
+        }
+        return new BadCommitException("Error operation class type of " + aClass
+            +" , operation failed from: " + node.nodeMsg().toString());
     }
     
-    public static BadCommitException classNotSupportException(Packer packer, Node node) {
-        return new BadCommitException("Error Operation type of " + packer.objClass() 
-            + " , Operation Failed from: " + node.nodeMsg().toString());
-    }
     public static BadCommitException commitNotSupportException(Node node, NodeMessage nodeMessage) {
+        if (nodeMessage == null) {
+            return new BadCommitException(
+                "Null commit message from: " + node.nodeMsg().toString());    
+        }
         return new BadCommitException(
-            "commit not support:" + nodeMessage.json().toString() +
+            "Commit not support:" + nodeMessage.json().toString() +
                 ", from: " + node.nodeMsg().toString());
     }
 }
