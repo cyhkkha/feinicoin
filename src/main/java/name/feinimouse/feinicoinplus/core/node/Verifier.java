e4ffb5d6a5af9172b8053da26a23ee1e4df97bfa
feinimouse <704602977@qq.com>
2020-01-01 17:48:18 +0800

    单独提出交易和资产交易的验证流程

--- src/main/java/name/feinimouse/feinicoinplus/core/node/Verifier.java
+++ src/main/java/name/feinimouse/feinicoinplus/core/node/Verifier.java
@@ -9,12 +9,14 @@ import name.feinimouse.feinicoinplus.core.block.Transaction;
 import name.feinimouse.feinicoinplus.core.SignGen;
 import name.feinimouse.feinicoinplus.core.data.*;
 import name.feinimouse.feinicoinplus.core.exception.BadCommitException;
+import name.feinimouse.feinicoinplus.core.exception.ControllableException;
 import name.feinimouse.feinicoinplus.core.lambda.InOutRunner;
 
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 // verifier基类
 //@Component("verifier")
@@ -56,21 +58,42 @@ public class Verifier extends CacheNode {
     @Override
     protected void resolveCache() {
         Optional.ofNullable(cacheWait.poll(Transaction.class))
-            .ifPresent(carrier -> resolveVerify(carrier, packer -> ((Transaction) packer.obj()).getSender()));
+            .ifPresent(carrier -> resolveVerify(carrier, this::resolveTransactionVerify));
         Optional.ofNullable(cacheWait.poll(AssetTrans.class))
-            .ifPresent(carrier -> resolveVerify(carrier, packer -> ((AssetTrans) packer.obj()).getOperator()));
+            .ifPresent(carrier -> resolveVerify(carrier, this::resolveAssetVerify));
     }
 
-    protected void resolveVerify(Carrier carrier, InOutRunner<Packer, String> runner) {
+    private boolean resolveAssetVerify(Packer packer) {
+        AssetTrans assetTrans = (AssetTrans) packer.obj();
+        boolean result;
+        {
+            String signer = assetTrans.getOperator();
+            PublicKey key = publicKeyHub.getKey(signer);
+            result = signGen.verify(key, packer, signer);
+        }
+        if (assetTrans.getTransaction() != null) {
+            String signer = assetTrans.getTransaction().getSender();
+            PublicKey key = publicKeyHub.getKey(signer);
+            result = result || signGen.verify(key, packer, signer);
+        }
+        return result;
+    }
+    
+    private boolean resolveTransactionVerify(Packer packer) {
+        Transaction trans = (Transaction) packer.obj();
+        String signer = trans.getSender();
+        PublicKey key = publicKeyHub.getKey(signer);
+        return signGen.verify(key, packer, signer);
+    }
+
+    protected void resolveVerify(Carrier carrier, InOutRunner<Packer, Boolean> verify) {
         Packer packer = carrier.getPacker();
         AttachMessage attachMessage = carrier.getAttachMessage();
         NodeMessage nodeMessage = carrier.getNodeMessage();
         String callbackAddress = nodeMessage.getCallback();
-        String signer = runner.run(packer);
         // 验证并签名
         attachMessage.setVerifier(address);
-        PublicKey key = publicKeyHub.getKey(signer);
-        attachMessage.setVerifiedResult(signGen.verify(key, packer, signer));
+        attachMessage.setVerifiedResult(verify.run(packer));
         signGen.sign(privateKey, packer, address);
         // 回调给Order
         Carrier nextCarrier = genCarrier(callbackAddress, MSG_CALLBACK_VERIFIER, attachMessage);
