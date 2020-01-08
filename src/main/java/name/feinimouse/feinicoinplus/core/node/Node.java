c11d1070a8a0cdc0f441ef0016b36bea1ca4ec9a
feinimouse <704602977@qq.com>
2020-01-08 21:46:11 +0800

    将公用的标志位放到接口中，将节点的地址和类型的获取放到接口中

--- src/main/java/name/feinimouse/feinicoinplus/core/node/Node.java
+++ src/main/java/name/feinimouse/feinicoinplus/core/node/Node.java
@@ -5,7 +5,19 @@ import name.feinimouse.feinicoinplus.exception.BadCommitException;
 import org.json.JSONObject;
 
 public interface Node {
+    int NODE_ORDER = 200;
+    int NODE_VERIFIER = 201;
+    int NODE_CENTER = 202;
+    int NODE_ENTER = 203;
+
+    int MSG_COMMIT_ORDER = 100;
+    int MSG_COMMIT_VERIFIER = 101;
+    int MSG_CALLBACK_VERIFIER = 102;
+    int MSG_FETCH_ORDER = 103;
+    int MSG_CALLBACK_ORDER = 104;
+    
     String getAddress();
+    int getNodeType();
     void commit(Carrier carrier) throws BadCommitException;
     Carrier fetch(Carrier carrier) throws BadCommitException;
     void stopNode();
