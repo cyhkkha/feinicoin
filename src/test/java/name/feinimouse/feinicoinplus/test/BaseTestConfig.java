bd7c8b9d92d276ca449ef639fcc378ee62913ab5
feinimouse <704602977@qq.com>
2020-01-08 21:48:53 +0800

    删去多于内容

---
+++ src/test/java/name/feinimouse/feinicoinplus/test/BaseTestConfig.java
@@ -0,0 +1,9 @@
+package name.feinimouse.feinicoinplus.test;
+
+import name.feinimouse.feinicoinplus.base.BaseConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@ComponentScan("name.feinimouse.feinicoinplus.test")
+public class BaseTestConfig extends BaseConfig {
+}
