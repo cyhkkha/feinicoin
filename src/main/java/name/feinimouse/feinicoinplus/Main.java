9bcd8c03a95d685c027a0ab277fe4bf12dcf4eb9
feinimouse <704602977@qq.com>
2020-01-08 21:48:17 +0800

    临时调整

--- src/main/java/name/feinimouse/feinicoinplus/Main.java
+++ src/main/java/name/feinimouse/feinicoinplus/Main.java
@@ -3,7 +3,6 @@ package name.feinimouse.feinicoinplus;
 import name.feinimouse.feinicoinplus.core.data.InitParam;
 import name.feinimouse.feinicoinplus.core.SimRunner;
 import name.feinimouse.feinicoinplus.core.sim.ResultManager;
-import name.feinimouse.feinicoinplus.test.BaseTestConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,15 +10,14 @@ public class Main {
     public static final String ENV_TEST_BASE = "TEST_BASE";
     public static final String ENV_TEST = "TEST";
     
-    @SuppressWarnings({"UnnecessaryLocalVariable", "ConstantConditions", "UnusedAssignment"})
     public static void main(String[] args) {
-        String ENV = ENV_TEST_BASE;
-        ResultManager resultM;
-        
-        if (ENV.equals(ENV_TEST_BASE)) {
-            ApplicationContext context = new AnnotationConfigApplicationContext(BaseTestConfig.class);
-            SimRunner runner = (SimRunner) context.getBean("simRunner");
-            resultM = runner.start(new InitParam());
-        }
+//        String ENV = ENV_TEST_BASE;
+//        ResultManager resultM;
+//        
+//        if (ENV.equals(ENV_TEST_BASE)) {
+//            ApplicationContext context = new AnnotationConfigApplicationContext(BaseTestConfig.class);
+//            SimRunner runner = (SimRunner) context.getBean("simRunner");
+//            resultM = runner.start(new InitParam());
+//        }
     }
 }
