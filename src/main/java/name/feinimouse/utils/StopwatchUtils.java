e6188abced3a957a7bee0e85a6a55fc7f7d964ab
feinimouse <704602977@qq.com>
2020-01-11 17:18:19 +0800

    增加注释

--- src/main/java/name/feinimouse/utils/StopwatchUtils.java
+++ src/main/java/name/feinimouse/utils/StopwatchUtils.java
@@ -6,37 +6,78 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * 该工具能够在一个 *时间段* 内不断地 *重复* 执行某个任务
+ * @author  Cao Feinimouse
+ */
 public class StopwatchUtils {
     public static final long DEFAULT_INTERVAL_TIME = 0;
 
+    /**
+     * 在一个时间段内重复执行一个无返回值的任务
+     * @param maxRunTime 时间段大小
+     * @param runner 任务的具体内容，这是一个lambda表达式
+     * @return 任务报告，包括任务的具体执行时间和每次子任务的执行时间
+     */
     public static Statistics run(long maxRunTime, OrdinaryRunner runner) {
         return run(maxRunTime, DEFAULT_INTERVAL_TIME, runner);
     }
 
+    /**
+     * 同上，可以设置任务执行间隔
+     * @param maxRunTime 同上
+     * @param intervalTime 每个任务执行的间隔时间
+     * @param runner 同上
+     * @return 同上
+     */
     public static Statistics run(long maxRunTime, long intervalTime, OrdinaryRunner runner) {
         return run(maxRunTime, intervalTime, stopper -> {
             runner.run();
         });
     }
 
+    /**
+     * 在一个时间段内重复执行一个 *有* 返回值的任务
+     * @param maxRunTime 同上
+     * @param runner 同上
+     * @param <T> 返回值的类型
+     * @return 包含每次任务的执行结果，以及任务报告
+     */
     public static <T> Result<T> run(long maxRunTime, ReturnRunner<T> runner) {
         return run(maxRunTime, DEFAULT_INTERVAL_TIME, runner);
     }
-
+    
+    /** 同上 */
     public static <T> Result<T> run(long maxRunTime, long intervalTime, ReturnRunner<T> runner) {
         return run(maxRunTime, intervalTime, stopper -> {
             return runner.run();
         });
     }
 
+    /**
+     * 在一个时间段内重复执行一个无返回值的任务，在任务途中可以中断
+     * RunnerStopper 是一个执行器，包含 stop() 方法，能够中断秒表的运行
+     * @param maxRunTime 同上
+     * @param runner 同上
+     * @return 同上
+     */
     public static Statistics run(long maxRunTime, InputRunner<RunnerStopper> runner) {
         return run(maxRunTime, DEFAULT_INTERVAL_TIME, runner);
     }
 
+    /**
+     * 在一个时间段内重复执行一个 *有* 返回值的任务，在任务途中可以中断
+     * RunnerStopper 是一个执行器，包含 stop() 方法，能够中断秒表的运行
+     * @param maxRunTime 同上
+     * @param runner 同上
+     * @param <T> 返回值的类型
+     * @return 包含每次任务的执行结果，以及任务报告
+     */
     public static <T> Result<T> run(long maxRunTime, CustomRunner<RunnerStopper, T> runner) {
         return run(maxRunTime, DEFAULT_INTERVAL_TIME, runner);
     }
 
+    /** 同上 */
     public static <T> Result<T> run(long maxRunTime, long intervalTime
         , CustomRunner<RunnerStopper, T> runner) {
         if (runner == null) {
@@ -50,6 +91,7 @@ public class StopwatchUtils {
         return new Result<>(statistics, list);
     }
 
+    /** 同上 */
     public static Statistics run(long maxRunTime, long intervalTime
         , InputRunner<RunnerStopper> runner) {
         if (runner == null) {
