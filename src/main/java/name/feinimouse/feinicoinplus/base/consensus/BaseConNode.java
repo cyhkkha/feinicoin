957901a1d0029f920f735725c4e91c9c6156f519
feinimouse <704602977@qq.com>
2020-01-30 17:11:23 +0800

    加入日志，修复线程安全问题

--- src/main/java/name/feinimouse/feinicoinplus/base/consensus/BaseConNode.java
+++ src/main/java/name/feinimouse/feinicoinplus/base/consensus/BaseConNode.java
@@ -7,11 +7,15 @@ import name.feinimouse.feinicoinplus.consensus.ConNode;
 import name.feinimouse.feinicoinplus.core.crypt.PublicKeyHub;
 import name.feinimouse.feinicoinplus.core.crypt.SignGenerator;
 import name.feinimouse.feinicoinplus.core.data.Packer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
 public class BaseConNode implements ConNode {
+    private Logger logger = LogManager.getLogger(BaseConNode.class);
+    
     @Setter
     @Getter
     protected String address;
@@ -30,10 +34,11 @@ public class BaseConNode implements ConNode {
 
     protected ConMessage nowTask;
     protected int taskId = 0;
+    protected long consensusStartTime;
     
     @Override
     public synchronized void consensus(ConMessage message) {
-        if (nowTask != null || message.getId() <= taskId) {
+        if (!isConfirm() || message.getId() <= taskId) {
             // 若为同一状态则返回
             return;
         }
@@ -42,18 +47,27 @@ public class BaseConNode implements ConNode {
         String center = packer.getCenter();
         PublicKey publicKey = publicKeyHub.getKey(center);
         if (signGenerator.verify(publicKey, packer, center)) {
-            nowTask = new ConMessage(message, TYPE_CALLBACK);
+            nowTask = new ConMessage(message, TYPE_CONFIRM);
+            ConMessage nextMessage = new ConMessage(message, TYPE_CALLBACK);
+            
             taskId = nowTask.getId();
+            
             nowTask.setSender(address);
+            nextMessage.setSender(address);
+            
             signGenerator.sign(privateKey, nowTask, address);
-            net.callback(nowTask);
+            signGenerator.sign(privateKey, nextMessage, address);
+            
+            logger.trace("节点 {} 共识（id: {}）开始", address, message.getId());
+            consensusStartTime = System.currentTimeMillis();
+            net.callback(nextMessage);
         }
     }
 
     @Override
     public void callback(ConMessage message) {
         // 是否正在同步状态，若不在状态则进入状态
-        if (nowTask == null && message.getId() > taskId) {
+        if (isConfirm() && message.getId() > taskId) {
             consensus(message);
         }
         // 是否和当前是同一个任务
@@ -68,9 +82,14 @@ public class BaseConNode implements ConNode {
             nowTask.putSign(sender, message.getSign(sender));
             // 如果积累的签名数量大于2/3则完成同步并通知全网
             if (nowTask.signSize() >= nodeNum * 2 / 3) {
-                nowTask.setType(TYPE_CONFIRM);
-                endRound(nowTask);
-                net.confirm(nowTask);
+                synchronized (this) {
+                    if (!isConfirm()) {
+                        endRound(nowTask);
+                        logger.trace("节点 {} 发送完全确认消息，耗时 {}ms"
+                            , address, System.currentTimeMillis() - consensusStartTime);
+                        net.confirm(nowTask);
+                    }
+                }
             }
         }
 
@@ -79,14 +98,16 @@ public class BaseConNode implements ConNode {
     @Override
     public synchronized void confirm(ConMessage message) {
         // 是否正在同步状态，若不在状态则进入状态
-        if (nowTask == null && message.getId() > taskId) {
+        if (isConfirm() && message.getId() > taskId) {
             consensus(message);
         }
         // 是否和当前是同一个任务
         if (!nowTask.equals(message)) {
             return;
         }
-        
+
+        logger.trace("节点 {} 收到完全确认消息（来自: {}），耗时 {}ms"
+            , address, message.getSender(), System.currentTimeMillis() - consensusStartTime);
         // 签名数量要大于2/3
         if (message.signSize() >= nodeNum * 2 / 3) {
             // 验证所有的签名
@@ -101,6 +122,8 @@ public class BaseConNode implements ConNode {
 
     @Override
     public void endRound(ConMessage message) {
+        logger.trace("节点 {} 完成共识, 耗时 {}ms", address, 
+            System.currentTimeMillis() - consensusStartTime);
         nowTask = null;
     }
 
