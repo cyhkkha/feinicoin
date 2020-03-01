670a53abac5b767fab6de83d65c57b715f0236de
feinimouse <704602977@qq.com>
2020-03-01 21:40:48 +0800

    Main函数运行

--- src/main/java/name/feinimouse/feinicoinplus/BaseRunner.java
+++ src/main/java/name/feinimouse/feinicoinplus/BaseRunner.java
@@ -1,20 +1,22 @@
 package name.feinimouse.feinicoinplus;
 
+import name.feinimouse.feinicoinplus.base.sim.BaseNodeManager;
 import name.feinimouse.feinicoinplus.core.SimRunner;
 import name.feinimouse.feinicoinplus.core.data.InitParam;
+import name.feinimouse.feinicoinplus.core.sim.NodeManager;
 import name.feinimouse.feinicoinplus.core.sim.ResultManager;
+import name.feinimouse.utils.TimerUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration
+@Configuration("simRunner")
 @ComponentScan("name.feinimouse.feinicoinplus.base")
 @PropertySource("classpath:feinicoinplus-config-test.properties")
 public class BaseRunner implements SimRunner {
-    @Override
-    public ResultManager start(InitParam initParam) {
-        return null;
-    }
     //////////////////////////////////////////////////////////////////
     // 可能会用到的的注解：
     // @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // 非单例的bean注解
@@ -22,5 +24,35 @@ public class BaseRunner implements SimRunner {
     // @PropertySource("classpath:feinicoinplus-config-test.properties")
     // @Value("${coin.hash.seed}")
     //////////////////////////////////////////////////////////////////
+
+    protected Logger logger = LogManager.getLogger(BaseRunner.class);
+    
+    protected NodeManager nodeManager;
+
+    @Autowired
+    public void setNodeManager(NodeManager nodeManager) {
+        this.nodeManager = nodeManager;
+    }
+
+    @Override
+    public ResultManager start(InitParam initParam) {
+        BaseNodeManager baseNodeManager = (BaseNodeManager) nodeManager;
+        baseNodeManager.startClassicalNode();
+        logger.info("节点已启动");
+
+        long time = TimerUtils.run(() -> {
+            baseNodeManager.sendRandomMixTransClassical(100, 0);
+            try {
+                baseNodeManager.getClassicalCenter().join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        logger.info("总计运行时间 {} ms", time);
+        return null;
+    }
+    
+
+    
 
 }
--- src/main/java/name/feinimouse/feinicoinplus/Main.java
+++ src/main/java/name/feinimouse/feinicoinplus/Main.java
@@ -1,23 +1,16 @@
 package name.feinimouse.feinicoinplus;
 
-import name.feinimouse.feinicoinplus.core.data.InitParam;
 import name.feinimouse.feinicoinplus.core.SimRunner;
-import name.feinimouse.feinicoinplus.core.sim.ResultManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-    public static final String ENV_TEST_BASE = "TEST_BASE";
-    public static final String ENV_TEST = "TEST";
     
     public static void main(String[] args) {
-//        String ENV = ENV_TEST_BASE;
-//        ResultManager resultM;
-//        
-//        if (ENV.equals(ENV_TEST_BASE)) {
-//            ApplicationContext context = new AnnotationConfigApplicationContext(BaseTestConfig.class);
-//            SimRunner runner = (SimRunner) context.getBean("simRunner");
-//            resultM = runner.start(new InitParam());
-//        }
+
+        // Base test
+        ApplicationContext context = new AnnotationConfigApplicationContext(BaseRunner.class);
+        SimRunner runner = (SimRunner) context.getBean("simRunner");
+        runner.start(null);
     }
 }
