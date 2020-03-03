3477f28406f51257e1129b1e35a104c19d413149
feinimouse <704602977@qq.com>
2020-03-03 15:25:44 +0800

    新的BFT共识测试通过

--- src/test/java/name/feinimouse/feinicoinplus/base/TestConsensus.java
+++ src/test/java/name/feinimouse/feinicoinplus/base/TestConsensus.java
@@ -1,9 +1,6 @@
 package name.feinimouse.feinicoinplus.base;
 
-import name.feinimouse.feinicoinplus.base.consensus.PBFTNet;
-import name.feinimouse.feinicoinplus.base.consensus.ClassicalConNode;
-import name.feinimouse.feinicoinplus.base.consensus.ConNodeNet;
-import name.feinimouse.feinicoinplus.base.consensus.PBFTConNode;
+import name.feinimouse.feinicoinplus.base.consensus.*;
 import name.feinimouse.feinicoinplus.consensus.BFTMessage;
 import name.feinimouse.feinicoinplus.consensus.ConMessage;
 import name.feinimouse.feinicoinplus.consensus.ConNode;
@@ -93,23 +90,44 @@ public class TestConsensus extends BaseTest {
     
     @Test
     public void testPBFT() throws InterruptedException {
-        PBFTNet PBFTNet = (PBFTNet) context.getBean("pbftNet");
+        PBFTNet pbftNet = (PBFTNet) context.getBean("pbftNet");
         LoopUtils.loop(20, () -> {
-            PBFTConNode pbftConNode = (PBFTConNode) context.getBean("pbftConNode");
-            PBFTNet.putNode(pbftConNode);
+            PBFTNode pbftNode = (PBFTNode) context.getBean("pbftNode");
+            pbftNet.putNode(pbftNode);
         });
         BFTMessage bftMessage = new BFTMessage();
-        bftMessage.setMessage("testssssss");
+        bftMessage.setMessage("test ss ss ss");
         long startTime = System.currentTimeMillis();
-        PBFTNet.start(bftMessage);
-        while (!PBFTNet.isConsensus()) {
+        pbftNet.start(bftMessage);
+        while (!pbftNet.isConsensus()) {
             Thread.sleep(10);
             if (System.currentTimeMillis() - startTime > 10 * 1000) {
                 break;
             }
         }
         logger.info("共识时间：{} ms", System.currentTimeMillis() - startTime);
-        PBFTNet.destroy();
+        pbftNet.destroy();
+    }
+
+    @Test
+    public void testDPOS_BFT() throws InterruptedException {
+        DPOS_BFTNet dpos_bftNet = (DPOS_BFTNet) context.getBean("dpos_bftNet");
+        LoopUtils.loop(20, () -> {
+            DPOS_BFTNode dpos_bftNode = (DPOS_BFTNode) context.getBean("dpos_bftNode");
+            dpos_bftNet.putNode(dpos_bftNode);
+        });
+        BFTMessage bftMessage = new BFTMessage();
+        bftMessage.setMessage("test ss ss ss");
+        long startTime = System.currentTimeMillis();
+        dpos_bftNet.start(bftMessage);
+        while (!dpos_bftNet.isConsensus()) {
+            Thread.sleep(10);
+            if (System.currentTimeMillis() - startTime > 10 * 1000) {
+                break;
+            }
+        }
+        logger.info("共识时间：{} ms", System.currentTimeMillis() - startTime);
+        dpos_bftNet.destroy();
     }
     
 }
